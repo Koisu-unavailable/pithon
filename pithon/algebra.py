9f02c5eae307ce559e02817135ec2d20894602fa
Jamil Alausa <koisuspotify@gmail.com>
2026-05-08 17:58:48 -0700

    init

--- pithon/algebra.py
+++ pithon/algebra.py
@@ -1,7 +1,7 @@
 import random as rdm
 import utils
-import satmathbot.genmath.ProblemGenerator as ProbGen
-import sympy
+import pithon.ProblemGenerator as ProbGen
+
 
 
 class AlgebraGen(ProbGen.ProblemGenerator):
@@ -11,10 +11,16 @@ class AlgebraGen(ProbGen.ProblemGenerator):
     def generate_problem(self):
         """Returns the path to an image of the generated problem"""
         solution = rdm.randint(0, 100)
-        sympy.
+        
         return utils.render_latex(r"\frac{a}{b}")
+    def gen_basic_linear(self, lower: int, upper: int):
+        """Generate a basic linear equation in the form y=mx+b. where y,m,x,b are all integers
 
-
+        Args:
+            lower (int): lower bound for answer
+            upper (int): upper bound for answer
+        """
+        solution = rdm.randint(lower, upper)
 if __name__ == "__main__":
     gen = AlgebraGen()
     gen.generate_problem()
